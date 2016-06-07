23fde741e51361d612c11423edbd3fd19556354f
aniversarioperu <yoni@aniversarioperu.me>
2016-06-06 22:34:32 -0500

    start parsing acts per ubigeo

--- onpe_crawler/spiders/quaker.py
+++ onpe_crawler/spiders/quaker.py
@@ -11,10 +11,29 @@ class QuakerSpider(scrapy.Spider):
     def start_requests(self):
         yield Request(url=self.start_url,
                       headers={'X-Requested-With': 'XMLHttpRequest'},
-                      callback=self.parse)
+                      callback=self.get_actas)
 
-    # curl -v 'https://resultadoselecciones2016.onpe.gob.pe/PRP2V2016/ajax.php' --data '_claas&_accion=displayMesas&ubigeo=040129&nroMesa=007246&tipoElec=10&page=1&pornumero=1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:46.0) Gecko/20100101 Firefox/46.0' -H 'X-Requested-With: XMLHttpRequest'
+    def get_actas(self, response):
+        ubigeo = '150301'
+        return [FormRequest(url=self.base_url,
+                           headers={'X-Requested-With': 'XMLHttpRequest'},
+                           formdata={
+                               '_clase': 'actas',
+                               '_accion': 'displayActas',
+                               'tipoElec': '10',
+                               'ubigeo': ubigeo,
+                               'actasPor': '3907',
+                               'ubigeoLocal': ubigeo,
+                               'page': 'undefined',
+                           },
+                           meta={'ubigeo': ubigeo},
+                           callback=self.parse)]
+
     def parse(self, response):
+        filename = "ubigeo_" + response.meta['ubigeo'] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+
         ubigeo = "040129"
         mesa = '007246'
         return [FormRequest(url=self.base_url,
